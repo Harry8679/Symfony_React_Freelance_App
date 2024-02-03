e8265c30f223e9b4d785c43ad36ce3b3f8475a67
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-03 16:33:47 +0100

    Invoice number: chrono added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,6 +14,8 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+        $chrono = 1;
+
         for ($c = 0; $c < 30; $c++) {
             $customer = new Customer();
             $customer->setFirstName($faker->firstName())
@@ -29,7 +31,9 @@ class AppFixtures extends Fixture
                         ->setSentAt($faker->dateTimeBetween('-6 months'))
                         ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
                         ->setCustomer($customer)
+                        ->setChrono($chrono)
                 ;
+                $chrono++;
                 $manager->persist($invoice);
             }
         }
